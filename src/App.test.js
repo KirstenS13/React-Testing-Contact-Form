3d8d5be99557a3440bf63f80baa8ec281bebb0a3
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-06-11 21:11:07 -0400

    finished 1st basic test, could not get second test to work, got third test working

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,38 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+//begin my tests
+test("renders first name input form without crashing", () => {
+  const { getByText } = render(<App />);
+
+  getByText(/first name/i);
+})
+
+//this isn't working at all
+/* test("can type in first name input", () => {
+  const { getAllByDisplayValue } = render(<App />);
+
+  const firstNameIn = getAllByDisplayValue("");
+
+  fireEvent.input(firstNameIn, "Joy");
+
+  expect(firstNameIn).toBe("Joy");
+  expect(firstNameIn).not.toBe("");
+}) */
+
+test("can submit form", () => {
+  const { getByTestId } = render(<App />);
+
+  const submitButton = getByTestId("submit")
+
+  fireEvent.submit(submitButton);
+})
+
+test("can type in input form", () => {
+  
+})
